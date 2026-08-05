7aad0f634a0e30cdccbf4578da2c7e6b81bac116
blockersiontko <i.settingidofirefox@gmail.com>
2026-08-05 11:32:24 +0200

    Initial commit

---
+++ identificators_pl/pesel.py
@@ -0,0 +1,6 @@
+import datetime
+
+class PeselValidator:
+
+    def pesel(self):
+        pass
---
+++ identificators_pl/polish_identificator.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class PolishIdentificator(ABC):
+
+    def __init__(self, number: str):
+        self.numer = number
+
+    @abstractmethod
+    def _checksum_valid(self) -> bool:
+        ...
+
+    @abstractmethod
+    def _expected_length(self) -> int:
+        ...
+
+    @property
+    def is_valid(self) -> bool:
+        return len(self.number) == self._expected_length() and self._checksum_valid()
