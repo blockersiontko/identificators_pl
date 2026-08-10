67c8725f46c245c38a3b2b58208c9d0a17052b0f
blockersiontko <i.settingidofirefox@gmail.com>
2026-08-10 12:29:47 +0200

    Added basic logic for validators

---
+++ identificators_pl/nip.py
@@ -0,0 +1,13 @@
+import datetime
+from polish_identificator import PolishIdentificator
+
+class NIPValidator(PolishIdentificator):
+
+    def nip(self):
+        pass
+
+    def _checksum_valid(self):
+        return super()._checksum_valid()
+
+    def _expected_length(self):
+        return super()._expected_length()
---
+++ identificators_pl/regon.py
@@ -0,0 +1,13 @@
+import datetime
+from polish_identificator import PolishIdentificator
+
+class REGONValidator(PolishIdentificator):
+
+    def regon(self):
+        pass
+
+    def _checksum_valid(self):
+        return super()._checksum_valid()
+
+    def _expected_length(self):
+        return super()._expected_length()
