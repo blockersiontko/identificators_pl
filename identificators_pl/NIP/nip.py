a746da1c523d3eeaddcd9f16f3b0b83071f8f3ee
blockersiontko <i.settingidofirefox@gmail.com>
2026-08-11 16:50:47 +0200

    Added REGON logic

--- identificators_pl/NIP/nip.py
+++ identificators_pl/NIP/nip.py
@@ -1,14 +1,16 @@
-import datetime
 from polish_identificator import PolishIdentificator
 
 
 class NIPValidator(PolishIdentificator):
 
-    def nip(self):
-        pass
+    WEIGHTS = (6, 5, 7, 2, 3, 4, 5, 6, 7)
 
-    def _checksum_valid(self):
-        return super()._checksum_valid()
+    def _checksum_valid(self) -> bool:
+            if not self.number.isdigit():
+                 return False
+            digits = [int(d) for d in self.number]
+            checksum = sum(w * d for w, d in zip(self.WEIGHTS, digits[:-1])) % 11
+            return checksum == digits[-1]
 
     def _expected_length(self):
-        return super()._expected_length()
+        return 10
--- identificators_pl/REGON/regon.py
+++ identificators_pl/REGON/regon.py
@@ -1,14 +1,26 @@
-import datetime
 from polish_identificator import PolishIdentificator
 
 
 class REGONValidator(PolishIdentificator):
 
-    def regon(self):
-        pass
+    WEIGHTS_9 = (8, 9, 2, 3, 4, 5, 6, 7)
+    WEIGHTS_14 = (2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8)
 
     def _checksum_valid(self):
-        return super()._checksum_valid()
+        if not self.number.isdigit():
+            return False
+        digits = [int(d) for d in self.number]
+        if len(self.number) == 9:
+            checksum = sum(w * d for w, d in zip(self.WEIGHTS_9, digits[:-1])) % 11
+        elif len(self.number) == 14:
+            checksum = sum(w * d for w, d in zip(self.WEIGHTS_14, digits[:-1])) % 11
+        else:
+            return False
+
+        if checksum == 10:
+            return digits[-1] == 0
+        
+        return checksum == digits[-1]
 
     def _expected_length(self):
-        return super()._expected_length()
+        return (9, 14)
